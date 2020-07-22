549315488e4ef415d6e83b1d667da2641233b415
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2020-07-22 23:03:22 +0200

    feat: change model, add valiadtion and bleu score

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models as models
 
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
-        super(EncoderCNN, self).__init__()
+        super().__init__()
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
@@ -22,30 +23,45 @@ class EncoderCNN(nn.Module):
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, dropout=0.5):
         super().__init__()
 
+        # attributes
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+
         # layers definition
-        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
-        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.fc = nn.Linear(in_features=hidden_size, out_features=vocab_size)
+        self.embed = nn.Embedding(
+            num_embeddings=self.vocab_size, embedding_dim=self.embed_size
+        )
+        self.lstm = nn.LSTM(
+            input_size=self.embed_size,
+            hidden_size=self.hidden_size,
+            num_layers=self.num_layers,
+            batch_first=True,
+            dropout=self.dropout,
+        )
+        self.fc = nn.Linear(in_features=self.hidden_size, out_features=self.vocab_size)
 
     def forward(self, features, captions):
 
         # make embeddings for captions
         # drop last word because we don't want to predict anything after <stop>
         # embeddings size = (batch_size, sequence_size-1, embed_size)
-        embeddings = self.embed(captions[:,:-1])
+        embeddings = self.embed(captions[:, :-1])
 
         # reshape features from encoder
         # features size = (batch_size, 1, embed_size)
         features = features.view(features.shape[0], 1, -1)
 
         # concatenate features form encoder with embedded captions
-        # concatenate on 2nd dimention (1 in Python) because 1st is batch size,
-        # 3rd is features (informations) and 2nd is ordered sequence
-        # features in the first position because lstm will predict first word from features only
-        # next model will take first word and some "history informations" from previous LSTM cell
+        # concatenate on 2nd dimmention (1 in Python) because 1st position is batch size,
+        # 3rd is features (information) and 2nd is ordered sequence
+        # features in the first position because LSTM will predict first word from features only
+        # next model will take first word and some "history information" from previous LSTM cell
         inputs = torch.cat([features, embeddings], dim=1)
 
         # get LSTM output (this is all we need)
@@ -56,7 +72,7 @@ class DecoderRNN(nn.Module):
 
         # decode LSTM output to vector of values of vocab_size
         # we need this for loss function
-        outputs = self.fc(x)
+        outputs = F.log_softmax(self.fc(x), dim=2)
 
         return outputs
 
@@ -71,12 +87,12 @@ class DecoderRNN(nn.Module):
         # for prediction batch_size = 1
         inputs = inputs.view(inputs.shape[0], 1, -1)
 
-        # loop until <stop> is predicted or caption = max_len
+        # loop until <stop> is predicted or caption == max_len
         while True:
-            # get LSTM output
+            # get LSTM output (first state is None)
             x, states = self.lstm(inputs, states)
             # decode LSTM output to vector of values of vocab_size
-            output = self.fc(x)
+            output = F.log_softmax(self.fc(x), dim=2)
             # index of largest value in that vector is most probable token
             _, max_id = torch.max(output, dim=2)
             # append that token do caption list
